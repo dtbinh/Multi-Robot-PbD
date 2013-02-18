8fa002f548db80e0d2fecbd50f39f241a2f3d4e3
Distributed Intelligence Laboratory <dilab@lap80.(none)>
2013-02-18 17:49:49 -0500

    Add record.py

---
+++ Codes/GMM:GMR_v2.0/dtw/record.py
@@ -0,0 +1,70 @@
+import sys
+from naoqi import ALProxy
+import motion
+from time import *
+
+IP = '10.26.210.61'
+PORT = 9559
+global mem, motion, redBallTracker, behav
+mem = ALProxy('ALMemory', IP, PORT)
+motion = ALProxy('ALMotion', IP, PORT)
+redBallTracker = ALProxy("ALRedBallTracker", IP, PORT)
+behav = ALProxy('ALBehaviorManager', IP, PORT)
+
+def headTouch():
+  headFront = mem.getData('Device/SubDeviceList/Head/Touch/Front/Sensor/Value')
+  headMiddle = mem.getData('Device/SubDeviceList/Head/Touch/Middle/Sensor/Value')
+  headRear = mem.getData('Device/SubDeviceList/Head/Touch/Rear/Sensor/Value')
+  return headFront or headMiddle or headRear
+
+def JointData():
+  #pprint.pprint(mem.getDataListName())
+  #stiffnessLists = [0.0, 0.0, 0.0, 0.0, 1.0, 1.0]
+  #timeLists = 2.0
+  #motion.stiffnessInterpolation('LArm', stiffnessLists, timeLists) 
+  LShoulderPitch = mem.getData('Device/SubDeviceList/LShoulderPitch/Position/Actuator/Value')
+  LShoulderRoll = mem.getData('Device/SubDeviceList/LShoulderRoll/Position/Actuator/Value')
+  LElbowYaw = mem.getData('Device/SubDeviceList/LElbowYaw/Position/Actuator/Value')
+  LElbowRoll = mem.getData('Device/SubDeviceList/LElbowRoll/Position/Actuator/Value')
+  LWristYaw = mem.getData('Device/SubDeviceList/LWristYaw/Position/Actuator/Value')
+  LHand = mem.getData('Device/SubDeviceList/LHand/Position/Sensor/Value')
+  # LHipYawPitch and RHipYawPitch share the same motor
+  Hip = mem.getData('Device/SubDeviceList/LHipYawPitch/Position/Sensor/Value')  
+  
+  result = [LShoulderPitch, LShoulderRoll, LElbowYaw, LElbowRoll, LWristYaw, LHand, Hip]
+  return str(result) + "\t"
+
+def HandData():
+  # 0-torso, 1-world, 2-robot
+  space = 0
+  useSensorValues = True
+  return str(motion.getPosition("LArm", space, useSensorValues)) + "\t"
+
+def BallData():
+  if not redBallTracker.isActive():
+    print "Tracker is not active."
+    sys.exit(1)
+  return str(redBallTracker.getPosition()) + "\n"
+
+####### main function ######
+filename = 'data/data_' + str(time())
+f = open(filename, 'w+')
+
+# set head stiffness for ball tracking
+motion.setStiffnesses("Head", 1.0)
+# start traking
+redBallTracker.startTracker()
+
+counter = 0
+while not headTouch():
+  Data = JointData()+HandData()+BallData()
+  print counter, Data
+  counter += 1
+  f.write(Data)
+f.close()
+
+#stop tracking
+redBallTracker.stopTracker()  
+# remove whole body stiffness
+motion.setStiffnesses('Body', 0)
+print "Exit recording."
