c2261c8824890f5b2001f3e5b59fab66c2e7e939
zhangchi8620 <czhang24@utk.edu>
2013-06-26 15:19:19 -0400

    gmr_py

---
+++ Codes/GMM_GMR_v2.0/gmr_python/EM_init_kmeans.py
@@ -0,0 +1,10 @@
+import sys
+import time
+import numpy as np
+
+def EM_init_kmeans(Data, nbStates):
+	nbVar = len(Data)
+	nbData = 
+
+
+	return Priors, Mu, Sigma
---
+++ Codes/GMM_GMR_v2.0/gmr_python/read_data.py
@@ -0,0 +1,5 @@
+def read_data(filename):
+	with open(filename) as f:
+		content = f.readlines()
+		print content
+	return
