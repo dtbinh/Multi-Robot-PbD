912f516f099dfbbbda6e6367055b6d32816a3dfb
Distributed Intelligence Laboratory <dilab@lap80.(none)>
2013-06-27 16:42:27 -0400

    all in control

--- Codes/GMM_GMR_v2.0/control/wiping_bk.py
+++ Codes/GMM_GMR_v2.0/control/wiping_bk.py
@@ -1,6 +1,8 @@
+import os
 import sys
 import time
 from naoqi import ALProxy
+import naoqi
 '''
 if (len(sys.argv) < 2):
     print "Usage: 'python motion_angleinterpolationwithspeed.py IP [PORT]'"
@@ -18,18 +20,19 @@ except Exception,e:
     print "Error was: ",e
     sys.exit(1)
 '''
-IP = '10.26.210.62' 
+IP = '10.26.210.60' 
 PORT = 9559
 proxy = ALProxy("ALMotion", IP, PORT)
 mem = ALProxy('ALMemory', IP, PORT)
+photo = ALProxy("ALVisionToolbox", IP, PORT)
 proxy.setStiffnesses("RArm", 1.0)
 
-#names  = ["RArm", "LHipYawPitch"]
+names  = ["RArm", "LHipPitch", "RHipPitch"]
 #names  = "RArm"
 #numJoints = len(proxy.getJointNames(names))
 #print "numJoints", numJoints
 
-filename = "../data/B/reproduced.txt"
+filename = "../data/wiping/reproduced.txt"
 f = open(filename)
 line = f.readlines()
 f.close
@@ -40,14 +43,18 @@ def headTouch():
   headRear = mem.getData('Device/SubDeviceList/Head/Touch/Rear/Sensor/Value')
   return headFront or headMiddle or headRear
 
+
 stiffnessLists = 1.0
 timeLists = 2.0
 proxy.stiffnessInterpolation(names, stiffnessLists, timeLists)
 
-proxy.setStiffnesses("LHand", 1.0)
-proxy.closeHand('LHand')
-proxy.setStiffnesses("LHand", 1.0)
+proxy.setStiffnesses("RHand", 1.0)
+proxy.closeHand('RHand')
+proxy.setStiffnesses("RHand", 1.0)
+
+'''
 for i in range(0, len(line)):
+  #photo.takePictureRegularly(1, "/tmp/cameras", 1, "jpg", 2)
   targetAngle = []
   #print i, line[i]
   targetAngle = line[i].split('\t')
@@ -58,11 +65,29 @@ for i in range(0, len(line)):
   if headTouch():
     break
 
-  new = targetAngle[0:7]
-  print "new: ",new
+  joints = targetAngle[0:8]
+  print "joints ",joints
+  #print "RArm ",joints[0:6]
+  #print "LHip ", joints[6]
+  #print "RHip ", joints[7]
   # Using 10% of maximum joint speed
-  maxSpeedFraction  = 0.05
-  proxy.angleInterpolationWithSpeed(names, new, maxSpeedFraction)
- 
+  maxSpeedFraction  = 0.1
+  #proxy.angleInterpolationWithSpeed(names, joints, maxSpeedFraction)
+  #proxy.setAngles(names, joints, maxSpeedFraction) #angleInterpolationWithSpeed works better than setAngles, regarding the speed
+'''
+photo.stopTPR("/tmp/cameras/test_","jpg")
+print "Start taking picture"
+print photo.backlighting()
+print photo.isItDark()
+try:
+  photo.takePictureRegularly(0.5, "/tmp/cameras/test_", True, "jpg", 0)
+except Exception, e:
+  print "Error recording"
+  print str(e)
+  exit(1) 
+#photo.takePicture()
+
+print "Finish taking picture"
+   
 proxy.openHand('LHand') 
 proxy.setStiffnesses("Body", 0.0)
--- Codes/GMM_GMR_v2.0/control/wiping_img.py
+++ Codes/GMM_GMR_v2.0/control/wiping_img.py
@@ -1,7 +1,10 @@
+import os
 import sys
 import time
-import motion 
 from naoqi import ALProxy
+import vision_definitions
+import Image
+ 
 '''
 if (len(sys.argv) < 2):
     print "Usage: 'python motion_angleinterpolationwithspeed.py IP [PORT]'"
@@ -23,7 +26,7 @@ IP = '10.26.210.60'
 PORT = 9559
 proxy = ALProxy("ALMotion", IP, PORT)
 mem = ALProxy('ALMemory', IP, PORT)
-motion = ALProxy('ALMotion', IP, PORT)
+cam = ALProxy("ALVideoDevice", IP, PORT)
 proxy.setStiffnesses("RArm", 1.0)
 
 names  = ["RArm", "LHipPitch", "RHipPitch"]
@@ -31,24 +34,50 @@ names  = ["RArm", "LHipPitch", "RHipPitch"]
 #numJoints = len(proxy.getJointNames(names))
 #print "numJoints", numJoints
 
-'''
 filename = "../data/wiping/reproduced.txt"
 f = open(filename)
 line = f.readlines()
 f.close
-'''
+
+def pictureCapture():
+  resolution = vision_definitions.kQQVGA
+  colorSpace = vision_definitions.kYUVColorSpace
+  fps = 30
+  nameID = cam.subscribe("python_GVM", resolution, colorSpace, fps)
+  print nameID
+  cam.setResolution(nameID, resolution)  
+
+  print "getting images in remote"
+  for i in range(0, 20):
+    cam.getImageRemote(nameID)
+  cam.unsubscribe(nameID)
+  print "end of gvm_getImageRemote python script"
+
+def pictureShow():
+  resolution = 2
+  colorSpace = 11
+
+  videoClient = cam.subscribe("python_client", resolution, colorSpace, 5)
+  t0 = time.time()
+  naoImage = cam.getImageRemote(videoClient) 
+  t1 = time.time()
+  print "acquistion delay", t1 - t0
+  cam.unsubscribe(videoClient)
+
+  imageWidth = naoImage[0]
+  imageHeight = naoImage[1]
+  array = naoImage[6]
+
+  im = Image.fromstring("RGB", (imageWidth, imageHeight), array)
+
+  im.save("camImage.png", "PNG")
+  im.show()
+
 def headTouch():
   headFront = mem.getData('Device/SubDeviceList/Head/Touch/Front/Sensor/Value')
   headMiddle = mem.getData('Device/SubDeviceList/Head/Touch/Middle/Sensor/Value')
   headRear = mem.getData('Device/SubDeviceList/Head/Touch/Rear/Sensor/Value')
   return headFront or headMiddle or headRear
-
-
-def HandData():
-  # 0-torso, 1-world, 2-robot
-  space = 0
-  useSensorValues = True
-  return motion.getPosition("RArm", space, useSensorValues)
 
 
 stiffnessLists = 1.0
@@ -58,8 +87,10 @@ proxy.stiffnessInterpolation(names, stiffnessLists, timeLists)
 proxy.setStiffnesses("RHand", 1.0)
 proxy.closeHand('RHand')
 proxy.setStiffnesses("RHand", 1.0)
+
 '''
 for i in range(0, len(line)):
+  #photo.takePictureRegularly(1, "/tmp/cameras", 1, "jpg", 2)
   targetAngle = []
   #print i, line[i]
   targetAngle = line[i].split('\t')
@@ -69,17 +100,33 @@ for i in range(0, len(line)):
   i = i + 1
   if headTouch():
     break
-'''
-while not headTouch():
-  hand = HandData()
-  print "current hand position ",hand
-  var = raw_input("Enter 8 DOF joints: ")
-  joints = map(float, var.split())
-  print "8 DOF is: ", joints 
+
+  joints = targetAngle[0:8]
+  print "joints ",joints
+  #print "RArm ",joints[0:6]
+  #print "LHip ", joints[6]
+  #print "RHip ", joints[7]
   # Using 10% of maximum joint speed
-  maxSpeedFraction  = 0.05
+  maxSpeedFraction  = 0.1
   #proxy.angleInterpolationWithSpeed(names, joints, maxSpeedFraction)
-  #proxy.setAngles(names, joints, maxSpeedFraction)
+  #proxy.setAngles(names, joints, maxSpeedFraction) #angleInterpolationWithSpeed works better than setAngles, regarding the speed
+'''
+#photo.stopTPR("/tmp/cameras/test_","jpg")
+#print "Start taking picture"
+#print photo.backlighting()
+#print photo.isItDark()
+#try:
+#  photo.takePictureRegularly(0.5, "/tmp/cameras/test_", True, "jpg", 0)
+#except Exception, e:
+#  print "Error recording"
+#  print str(e)
+#  exit(1) 
+#photo.takePicture()
+
+#pictureCapture()
+naoImage = pictureShow()
+
+print "Finish taking picture"
    
 proxy.openHand('LHand') 
 proxy.setStiffnesses("Body", 0.0)
