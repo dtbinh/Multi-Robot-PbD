d70834f8c83f52d5f47a4788a131c3c363780388
Distributed Intelligence Laboratory <dilab@lap80.(none)>
2013-06-28 15:20:25 -0400

    all

--- Codes/GMM_GMR_v2.0/control/wiping.py
+++ Codes/GMM_GMR_v2.0/control/wiping.py
@@ -1,65 +1,93 @@
+import os
 import sys
 import time
-import motion 
 from naoqi import ALProxy
-'''
-if (len(sys.argv) < 2):
-    print "Usage: 'python motion_angleinterpolationwithspeed.py IP [PORT]'"
-    sys.exit(1)
-
-IP = '10.26.210.61' 
-PORT = 9559
-if (len(sys.argv) > 2):
-    PORT = sys.argv[2]
-try:
-    proxy = ALProxy("ALMotion", IP, PORT)
-    mem = ALProxy('ALMemory', IP, PORT)
-except Exception,e:
-    print "Could not create proxy to ALMotion"
-    print "Error was: ",e
-    sys.exit(1)
-'''
-IP = '10.26.210.60' 
-PORT = 9559
-proxy = ALProxy("ALMotion", IP, PORT)
-mem = ALProxy('ALMemory', IP, PORT)
-motion = ALProxy('ALMotion', IP, PORT)
-proxy.setStiffnesses("RArm", 1.0)
-
-names  = ["RArm", "LHipPitch", "RHipPitch"]
-#names  = "RArm"
-#numJoints = len(proxy.getJointNames(names))
-#print "numJoints", numJoints
-
-'''
-filename = "../data/wiping/reproduced.txt"
-f = open(filename)
-line = f.readlines()
-f.close
-'''
-def headTouch():
-  headFront = mem.getData('Device/SubDeviceList/Head/Touch/Front/Sensor/Value')
-  headMiddle = mem.getData('Device/SubDeviceList/Head/Touch/Middle/Sensor/Value')
-  headRear = mem.getData('Device/SubDeviceList/Head/Touch/Rear/Sensor/Value')
-  return headFront or headMiddle or headRear
+import vision_definitions
+import Image
+ 
+class  ROBOT():
+  def __init__(self, ip, port, side):
+    self.ip = ip
+    self.port = port
+    try:
+      self.motion = ALProxy("ALMotion", self.ip, self.port)
+      self.memory = ALProxy('ALMemory', self.ip, self.port)
+      self.camera = ALProxy("ALVideoDevice", self.ip, self.port)
+      self.speech = ALProxy("ALTextToSpeech", self.ip, self.port)
+    except Exception, e:
+      print "Could not create proxy"
+      print "Error: ", e
+      sys.exit(1)
+  
+  def headTouch(self):
+    headFront = mem.getData('Device/SubDeviceList/Head/Touch/Front/Sensor/Value')
+    headMiddle = mem.getData('Device/SubDeviceList/Head/Touch/Middle/Sensor/Value')
+    headRear = mem.getData('Device/SubDeviceList/Head/Touch/Rear/Sensor/Value')
+    return headFront or headMiddle or headRear
 
 
-def HandData():
-  # 0-torso, 1-world, 2-robot
-  space = 0
-  useSensorValues = True
-  return motion.getPosition("RArm", space, useSensorValues)
+  def pictureCapture(self):
+    resolution = vision_definitions.kQQVGA
+    colorSpace = vision_definitions.kYUVColorSpace
+    fps = 30
+    nameID = self.camera.subscribe("python_GVM", resolution, colorSpace, fps)
+    print nameID
+    self.camera.setResolution(nameID, resolution)  
+
+    print "getting images in remote"
+    for i in range(0, 20):
+      self.camera.getImageRemote(nameID)
+    
+    self.camera.unsubscribe(nameID)
+    print "end of gvm_getImageRemote python script"
+
+  def pictureShow(self):
+    resolution = 2
+    colorSpace = 11
+    videoClient = self.camera.subscribe("python_client", resolution, colorSpace, 5)
+    
+    t0 = time.time()
+    naoImage = self.camera.getImageRemote(videoClient) 
+    t1 = time.time()
+    print "acquistion delay", t1 - t0
+    self.camera.unsubscribe(videoClient)
+
+    imageWidth = naoImage[0]
+    imageHeight = naoImage[1]
+    array = naoImage[6]
+    im = Image.fromstring("RGB", (imageWidth, imageHeight), array)
+
+    im.save("../data/wiping/img/camImage.png", "PNG")
+    im.show()
+
+####### main ########
+if __name__ == '__main__':
+  ironhide = ROBOT('10.26.210.60', 9559, 'R') 
+  
+  #ironhide.motion.setStiffnesses("RArm", 1.0)
+
+  #names  = ["RArm", "LHipPitch", "RHipPitch"]
+  #names  = "RArm"
+  #numJoints = len(proxy.getJointNames(names))
+  #print "numJoints", numJoints
+
+  #filename = "../data/wiping/reproduced.txt"
+  #f = open(filename)
+  #line = f.readlines()
+  #f.close
+  '''
+  stiffnessLists = 1.0
+  timeLists = 2.0
+  proxy.stiffnessInterpolation(names, stiffnessLists, timeLists)
 
 
-stiffnessLists = 1.0
-timeLists = 2.0
-proxy.stiffnessInterpolation(names, stiffnessLists, timeLists)
+  proxy.setStiffnesses("RHand", 1.0)
+  proxy.closeHand('RHand')
+  proxy.setStiffnesses("RHand", 1.0)
 
-proxy.setStiffnesses("RHand", 1.0)
-proxy.closeHand('RHand')
-proxy.setStiffnesses("RHand", 1.0)
-'''
+
 for i in range(0, len(line)):
+  #photo.takePictureRegularly(1, "/tmp/cameras", 1, "jpg", 2)
   targetAngle = []
   #print i, line[i]
   targetAngle = line[i].split('\t')
@@ -69,17 +97,23 @@ for i in range(0, len(line)):
   i = i + 1
   if headTouch():
     break
-'''
-while not headTouch():
-  hand = HandData()
-  print "current hand position ",hand
-  var = raw_input("Enter 8 DOF joints: ")
-  joints = map(float, var.split())
-  print "8 DOF is: ", joints 
+
+  joints = targetAngle[0:8]
+  print "joints ",joints
+  #print "RArm ",joints[0:6]
+  #print "LHip ", joints[6]
+  #print "RHip ", joints[7]
   # Using 10% of maximum joint speed
-  maxSpeedFraction  = 0.05
+  maxSpeedFraction  = 0.1
   #proxy.angleInterpolationWithSpeed(names, joints, maxSpeedFraction)
-  #proxy.setAngles(names, joints, maxSpeedFraction)
+  #proxy.setAngles(names, joints, maxSpeedFraction) #angleInterpolationWithSpeed works better than setAngles, regarding the speed
+'''
+  ironhide.pictureCapture()
+  ironhide.pictureShow()
+
+  print "Finish taking picture"
    
-proxy.openHand('LHand') 
-proxy.setStiffnesses("Body", 0.0)
+  ironhide.motion.openHand('LHand') 
+  ironhide.motion.setStiffnesses("Body", 0.0)
+  
+  print "Exit normally"
