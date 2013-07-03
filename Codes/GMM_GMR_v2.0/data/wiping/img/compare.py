fd4201fe2ee26c5e9969b7f9e0e6071837ae87ae
zhangchi8620 <czhang24@utk.edu>
2013-07-03 15:33:09 -0400

    new compare.py

--- Codes/GMM_GMR_v2.0/data/wiping/img/compare.py
+++ Codes/GMM_GMR_v2.0/data/wiping/img/compare.py
@@ -1,15 +1,20 @@
+import Image
+import ImageChops
+import os, sys
 import math, operator
-from PIL import Image
+import ImageFilter
+	
 def compare(file1, file2):
-    image1 = Image.open(file1)
-    image2 = Image.open(file2)
-    h1 = image1.histogram()
-    h2 = image2.histogram()
-    rms = math.sqrt(reduce(operator.add,
-    map(lambda a,b: (a-b)**2, h1, h2))/len(h1))
-    return rms
+	im1 = Image.open(file1)
+	im2 = Image.open(file2)
 
-if __name__=='__main__':
-    import sys
-    file1, file2 = sys.argv[1:]
-    print compare(file1, file2)
+	h1 = im1.convert('L').histogram()
+	h2 = im2.convert('L').histogram()
+	
+	rms = math.sqrt(reduce(operator.add,
+	map(lambda a,b: (a-b)**2, h1, h2))/len(h1))
+ 	return rms	
+
+if __name__ == '__main__':
+	file1, file2 = sys.argv[1:]
+	print compare(file1, file2)
