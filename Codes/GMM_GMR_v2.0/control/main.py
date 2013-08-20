98edb8fb6987aabd89f25066001e6065a48221d4
zhangchi8620 <czhang24@utk.edu>
2013-08-19 20:27:14 -0400

    remove old files

--- Codes/GMM_GMR_v2.0/control/main.py
+++
@@ -1,9 +0,0 @@
-import os, sys
-from compare import compare
-from action import action
-
-if __name__ == "__main__":
-  ironhide = "10.26.210.60"
-  action(ironhide, False, "wiping")
-  diff = compare()
-  print "compared difference: ", diff
