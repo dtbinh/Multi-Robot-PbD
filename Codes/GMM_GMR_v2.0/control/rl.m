f018b137a44f6166278c6fca5482be3982a6f980
zhangchi8620 <czhang24@utk.edu>
2013-09-02 12:36:14 -0400

    new rl.m, unfinished

--- Codes/GMM_GMR_v2.0/control/rl.m
+++ Codes/GMM_GMR_v2.0/control/rl.m
@@ -1,25 +1,66 @@
-function Mu = rl(Mu, score)
+function Mu = rl(Mu, DoFs)
     [nbVar, nbState] = size(Mu);
-    %for i = 1 : length(score)
-    for i = 1 : 1
-        GMMStateIndex = score(i);
-        state = MU(1+GMMStateIndex, :);
-        step  = (max(sate) - min(state)) / nbState;
+    nbDofs = length(DoFs)
+    state = zeros(length(DoFs), nbState);
+    step = zeros(length(DoFs),1);
     
-        Mu(i, : ) = Mu(i, :) + step
+    for i = 1 : length(DoFs)
+        state(i, :) = Mu(1+DoFs(i), :);
+        step(i)  = (max(state(i, :)) - min(state(i, :))) / nbState;
+        
+    end
+    step
+    state
+    
+    % training
+    state_new = zeros(size(state));
+    % each episode
+    for episode = 1 : 1000
+        % each step in one episode (update one data point)
+        for i = 1 : size(state, 1)
+            for j = 1 : size(state, 2)          
+                % take action a' using greedy plicy
+                a_new = greedy();
+                % observe state_new
+                state_new(i, j) = act(state(i, j), a_new, step(i));
+                % compute reward
+                rwd = reward(state_new(i,j));
+
+                delta = rwd + gamma * Q[state_new][a_new] - Q[state][a];
+                e(state, a) = gamma * lambda * e(state, a); 
+                % update Q, e
+                
+                
+            end
+        end
     end
     
-    save('data/Mu.mat', 'Mu');
+    
+            %Mu(i, : ) = Mu(i, :) + step
+
+    %save('data/Mu.mat', 'Mu');
 end
 
-goal = 0.32;
-
-function r = reward(joint, goal)
-    hand = testForwardKinect(joint);
-    % estimated hand pos between [goal-0.5, goal+0.5]
-    if hand(3) < goal+0.005 && hand(3) > goal - 0.005
-        r = 1.0
-    else
-        r = 0
+function state_new = act(state, a, step)
+    for i = 1 : size(state,1)
+        for j = 1 : size(state, 2)
+            if a == 1
+                state_new = state(i, j) + step
+            elseif a == -1
+                state_new = state(i, j) - step
+            end
+        end
     end
 end
+
+% goal = 0.32;
+% 
+% function r = reward(joint, goal)
+%     hand = testForwardKinect(joint);
+%     % estimated hand pos between [goal-0.5, goal+0.5]
+%     if hand(3) < goal+0.005 && hand(3) > goal - 0.005
+%         r = 1.0
+%     else
+%         r = 0
+%     end
+% end
